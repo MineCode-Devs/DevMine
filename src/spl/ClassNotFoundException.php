9d7878bd828a5cf0b4af9b67bae74d79786d8396
remote_vase <remotevase@gmail.com>
2017-01-09 20:39:51 -0500

    SOMEONE HELP
    @Taki21 @Ad5001 @TheDragonRing @AmiroEmad @applqpak SEE #13!

--- src/spl/ClassNotFoundException.php
+++ src/spl/ClassNotFoundException.php
@@ -1,19 +1,6 @@
 <?php
 
-/*
- * DevMine Standard PHP Library
- * Copyright (C) 2014-2016 Mostly by PocketMine team, modified by DevMine Team <https://github.com/DevMine/DevMine-SPL>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
-*/
+
 
 class ClassNotFoundException extends LogicException{
 
